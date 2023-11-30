e7586c50d5f5c3d39104b0d03c3dc4adbac2222f
alasaidi <alasaidi55@gmail.com>
2023-11-30 13:21:45 +0100

    setting up the menu and the background videos

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu");
   menu.classList.toggle("active");
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("active");
 }
